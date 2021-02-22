cb0a81c03380a1729ab9bd68e686c6bc3a63f49b
choct155 <choct155@gmail.com>
2021-02-21 19:47:34 -0500

    Initial commit that holds the basic structure. Forgot I was on the main branch, so I added a folder that should have been added on a branch. But, it's going there eventually anyway.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    description="Generic utility functionality for Python",
+    maintainer="Marvin Ward Jr",
+    name="pyutils",
+    version="0.0.1",
+    url="https://github.com/choct155/pyutils/tree/main"
+)
---
+++ utils/pycollections/list .py
@@ -0,0 +1,6 @@
+from typing import List, TypeVar, Callable
+
+
+A = TypeVar("A")
+B = TypeVar("B")
+
